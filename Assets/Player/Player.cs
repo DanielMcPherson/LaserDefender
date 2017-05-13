ec5d3f386b1e39fbd073f222ca5d0418205fa563
DanielMcPherson <mcphersond@gmail.com>
2017-05-12 20:35:26 -0400

    Initial code
    Basic menu system. Ship moves with arrow keys.

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float speed = 5f;
+    float xMin, xMax;
+
+    Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+        float padding = GetComponent<SpriteRenderer>().bounds.extents.x;
+
+        float cameraDistance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, cameraDistance));
+        Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, cameraDistance));
+        xMin = leftMost.x + padding;
+        xMax = rightMost.x - padding;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        Vector3 pos = rb.transform.position;
+        float moveX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        pos.x = Mathf.Clamp(pos.x + moveX, xMin, xMax);
+        rb.transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string level) {
+        Application.LoadLevel(level);
+    }
+
+    public void LoadNextLevel() {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+    
+    public void Quit() {
+        print("User asked to quit");
+        //Application.Quit();
+    }
+}
