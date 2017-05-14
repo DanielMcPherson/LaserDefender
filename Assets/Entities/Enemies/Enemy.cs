6bebbf651bac0c8d3a46f02b99d57f9941bfadfd
DanielMcPherson <mcphersond@gmail.com>
2017-05-14 13:25:37 -0400

    Player lasers destroy enemies
    Player lasers destroy enemies.

---
+++ Assets/Entities/Enemies/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    public float health = 250;
+
+    // Use this for initialization
+    void Start () {
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    
+    }
+
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Projectile laser = other.GetComponent<Projectile>();
+        if (laser) {
+            health -= laser.GetDamage();
+            laser.Hit();
+            if (health <= 0f) {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- Assets/Scripts/Shredder.cs
+++ Assets/Scripts/Shredder.cs
@@ -2,13 +2,13 @@
 using System.Collections;
 
 public class Shredder : MonoBehaviour {
-	/// <summary>
-	/// Sent when another object enters a trigger collider attached to this
-	/// object (2D physics only).
-	/// </summary>
-	/// <param name="other">The other Collider2D involved in this collision.</param>
-	void OnTriggerEnter2D(Collider2D other)
-	{
-		Destroy(other.gameObject);
-	}
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+    }
 }
