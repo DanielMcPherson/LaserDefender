d33c820a8cf2976ae4fd1036e3117f09017daa42
DanielMcPherson <mcphersond@gmail.com>
2017-05-19 20:20:06 -0400

    Destory particle effects after time
    Added a script to destroy object after specified time to prevent enemy
    hit and dead effects from accumulating and sticking around forever.

---
+++ Assets/Scripts/DestroyAfterTime.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAfterTime : MonoBehaviour {
+
+    [SerializeField] float destroyTime = 2f;
+    // Use this for initialization
+    void Start () {
+        Destroy(gameObject, destroyTime);
+    }
+}
